b140415aa5d842b3c7338a7c5798e3f4a6c4f055
Abimael Velasco <113620636+Abidola-Velasco@users.noreply.github.com>
2023-02-09 12:26:06 -0500

    Add files via upload

--- pygame/click_game.py
+++ pygame/click_game.py
@@ -4,12 +4,28 @@ Nugget.pos = 100, 50
 WIDTH = 500
 HEIGHT = Nugget.height + 20
 
+
 def draw():
     screen.clear()
-    screen.fill((255, 255, 255))
+    screen.fill((255, 0, 0))
     Nugget.draw()
 
+
 def update():
-    Nugget.left = Nugget.left +2
+    Nugget.left = Nugget.left + 2
     if Nugget.left > WIDTH:
         Nugget.right = 0
+
+
+Nugget.topright = 0, 10
+
+
+def on_mouse_down(pos):
+    if Nugget.collidepoint(pos):
+        set_character()
+
+
+def set_character():
+    Nugget.image = "charactertophat"
+    sounds.eep.play()
+    clock.schedule_unique(set_character, 1.0)
